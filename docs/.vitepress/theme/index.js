52100c83dd78e9e61806c363800a4c008948675e
TheCrazy8 <bennettbashford@gmail.com>
2025-09-19 16:23:28 -0400

    Remove team members data from index.js
    Removed VPTeamMembers and team members data from theme index.

--- docs/.vitepress/theme/index.js
+++ docs/.vitepress/theme/index.js
@@ -1,17 +1,4 @@
 import DefaultTheme from 'vitepress/theme'
-import { VPTeamMembers } from 'vitepress/theme'
-
-const members = [
-  {
-    avatar: 'https://avatars.githubusercontent.com/u/187664439?v=4',
-    name: 'TheCrazy8',
-    title: 'Creator',
-    links: [
-      { icon: 'github', link: 'https://github.com/TheCrazy8' }
-    ]
-  },
-  ...
-]
 
 export default {
   ...DefaultTheme
