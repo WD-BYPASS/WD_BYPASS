b54cb76edd606ad71577c81e4a2dd480a6da9eb7
TheCrazy8 <bennettbashford@gmail.com>
2025-09-23 08:39:18 -0400

    Add files via upload

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,45 @@
+export default {
+    title: "WD_Bypass Website",
+    description: "The website for WD_Bypass.",
+    base: "/WD_BYPASS/",
+    themeConfig: {
+        siteTitle: "WD_Bypass Website",
+        footer: {
+            message: "Released under the CC BY-NC-ND 4.0 License.",
+            copyright: "Copyright © 2025-present WD_Bypass",
+        },
+        nav: [
+            { text: 'Download', link: '/download' },
+            { text: 'Guide', link: '/guide' },
+            { text: 'Info', link: '/info' },
+            { text: 'Rules', link: '/rules' }
+        ],
+        sidebar: {
+            text: 'Pages',
+            items: [
+            { text: 'Download', link: '/download' },
+            { text: 'Guide', link: '/guide' },
+            { text: 'Info', link: '/info' },
+            { text: 'Rules', link: '/rules' }
+            ]
+        },
+
+        socialLinks: [
+            { icon: "github", link: "https://github.com/WD-BYPASS" },
+        ],
+
+        lastUpdated: {
+              text: 'Updated at',
+              formatOptions: {
+                    dateStyle: 'full',
+                    timeStyle: 'medium'
+              }
+        },
+        search: {
+              provider: 'local'
+        },
+    },
+    head: [
+        ['link', { rel: 'icon', type: 'image/x-icon', href: 'favicon.ico' }]
+    ],
+};
